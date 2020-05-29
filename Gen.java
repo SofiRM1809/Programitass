216d1a58d512421c552ccb78b99e1683e6b592fa
Sofi Ramirez <62312917+SofiRM1809@users.noreply.github.com>
2020-05-28 23:54:12 -0500

    Add files via upload

---
+++ Gen.java
@@ -0,0 +1,12 @@
+package pilas;
+
+public class Gen <T>{
+    private T valor;
+    public Gen(T valor){
+        this.valor = valor;
+    }
+    
+    public T obtenerValor(){
+        return this.valor;
+    }
+}
